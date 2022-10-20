135ff5117f8688379c750a4b1c22d9d5153c1010
uyerra12 <98996518+uyerra12@users.noreply.github.com>
2022-10-19 21:54:53 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install nodejs
-node -v
-sudo apt install npm
-npm install -g wrangler
+#sudo apt update
+#sudo apt install nodejs
+#node -v
+#sudo apt install npm
+#npm install -g wrangler
 
 
 echo "$CF_WORKER_ADMIN_KEY"
